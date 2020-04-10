1d8d81be2765611526f21134443ab817df5404aa
Nikolay Apanasik <n.apanasik@vigbo.com>
2020-04-10 11:01:21 +0300

    chore: Button stories

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
+import clsx from "clsx"
 
 import styles from "./button.module.scss"
 
-export function Button({ children, disabled, name, type, value, loading, ...props }) {
+export function Button({ children, disabled, name, type, value, loading, size, ...props }) {
+  console.log(disabled)
   return (
-    <button className={styles.button} {...{ disabled, name, type, value, loading }} {...props}>
+    <button
+      className={clsx(styles.button, {
+        [styles.disabled]: disabled,
+        [styles.small]: size === "small",
+        [styles.huge]: size === "huge",
+      })}
+      {...{ disabled, name, type, value, loading }}
+      {...props}
+    >
       {children}
     </button>
   )
@@ -18,4 +28,5 @@ Button.propTypes = {
   type: PropTypes.string,
   value: PropTypes.string,
   loading: PropTypes.bool,
+  size: PropTypes.string,
 }
--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,15 +1,28 @@
 import React from "react"
+import { withKnobs, text, boolean, select } from "@storybook/addon-knobs"
 import { action } from "@storybook/addon-actions"
-import { Button } from "components/button"
 
-export default {
-  title: "Button",
-  component: Button,
+import { Button as ButtonComponent } from "components/button"
+
+export function Button() {
+  const label = "Size"
+  const options = {
+    default: null,
+    small: "small",
+    huge: "huge",
+  }
+  const defaultValue = "default"
+
+  const value = select(label, options, defaultValue)
+
+  return (
+    <ButtonComponent onClick={action("clicked")} disabled={boolean("Disabled", false)} size={value}>
+      {text("Label", "Push me please")}
+    </ButtonComponent>
+  )
 }
 
-export const DefaultButton = () => <Button onClick={action("clicked")}>Hello Button</Button>
-export const DisabledButton = () => (
-  <Button onClick={action("clicked")} disabled>
-    Disabled Button
-  </Button>
-)
+export default {
+  title: "Components",
+  decorators: [withKnobs],
+}
