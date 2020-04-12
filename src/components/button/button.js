80a08c4f8ca33f7fbcb7422f5f31e0e36c2b586a
Nikolay Apanasik <n.apanasik@vigbo.com>
2020-04-12 21:09:40 +0300

    chore: Button improvements

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -5,12 +5,10 @@ import clsx from "clsx"
 import styles from "./button.module.scss"
 
 export function Button({ children, disabled, name, type, value, loading, size, ...props }) {
-  console.log(disabled)
   return (
     <button
       className={clsx(styles.button, {
         [styles.disabled]: disabled,
-        [styles.small]: size === "small",
         [styles.huge]: size === "huge",
       })}
       {...{ disabled, name, type, value, loading }}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
-import { Button } from "components/button"
 
+import { Modal } from "components/modal"
+import { Button } from "components/button"
 import "assets/styles/main.scss"
 
 function App() {
-  return <Button>Я обычная кнопка</Button>
+  const [open, setOpen] = useState(false)
+  function handleOpenClick() {
+    setOpen(true)
+  }
+  function handleCloseClick(e) {
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <button onClick={handleOpenClick}>+</button>
+      <p>
+        На ближайшую перспективу время стартапов закончилось: инвесторы скорее откажутся от поддержки, если у проектов
+        много долгов и они убыточны. Такое мнение высказал главный управляющий директор «Альфа-банка» Владимир
+        Верхошинский в интервью «Ведомостям». «То, что случилось с “Рокетбанком”, — это первый звоночек. Подобных
+        историй будет происходить больше — и в России, и в мире», — заявил Верхошинский. В марте материнская компания
+        финтех-сервиса Qiwi объявила о его закрытии до конца 2020 года. В Qiwi объяснили, что не смогли найти покупателя
+        на проект. Топ-менеджер отметил, что подобных историй будет больше — они связаны с демпинговой моделью цифрового
+        бизнеса стартапов.
+      </p>
+
+      <Modal handleOpenClick={handleOpenClick} handleCloseClick={handleCloseClick} open={open}>
+        <button onClick={handleCloseClick}>x</button>
+        <p>
+          На ближайшую перспективу время стартапов закончилось: инвесторы скорее откажутся от поддержки, если у проектов
+          много долгов и они убыточны. Такое мнение высказал главный управляющий директор «Альфа-банка» Владимир
+          Верхошинский в интервью «Ведомостям».
+        </p>
+        <Button size="small">Click me!</Button>
+      </Modal>
+    </>
+  )
 }
 
 ReactDOM.render(
