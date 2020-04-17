15bf6d463e45424b453bb678e6354419ebac7485
Nikolay Apanasik <n.apanasik@vigbo.com>
2020-04-17 22:18:01 +0300

    feat: Add global styles

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom"
 import { ThemeProvider } from "styled-components"
 import { theme } from "assets/styles/theme"
 
+import { GlobalStyle } from "assets/styles/global"
 import { Button } from "components/button"
 
 function App() {
-  return <Button>++</Button>
+  return <Button size="small">Testove button</Button>
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle></GlobalStyle>
       <App></App>
     </ThemeProvider>
   </React.StrictMode>,
