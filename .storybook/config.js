6705ffcbdcdc894ebdf04e74360053b43999b9bc
Nikolay Apanasik <n.apanasik@vigbo.com>
2020-04-19 22:49:40 +0300

    feat: Background selector

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,31 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
-import { addDecorator } from "@storybook/react"
+import { addDecorator, addParameters } from "@storybook/react"
 
 import { GlobalStyle } from "src/assets/styles/global"
 import { theme } from "src/assets/styles/theme"
 
+// +Global styles and theme
 addDecorator((storyFn) => (
   <React.Fragment>
     <GlobalStyle></GlobalStyle>
     <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
   </React.Fragment>
 ))
+// -Global styles and theme
+
+// +Preview backgrounds
+const themeColors = [
+  ...Object.entries(theme.colors.base),
+  ...Object.entries(theme.colors.grey),
+].reduce((acc, current) => {
+  const [name, value] = current
+  return [...acc, { name, value }]
+}, [])
+
+const backgrounds = [{ name: "white", value: "#fff", default: true }]
+
+addParameters({
+  backgrounds: [...backgrounds, ...themeColors],
+})
+// -Preview backgrounds
